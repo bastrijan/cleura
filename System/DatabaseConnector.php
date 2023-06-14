56c1d8fc0afa23a1b3642fadf3ecfbe0513233b1
Bastrijan <bastrijan@gmail.com>
2023-06-14 20:12:23 +0200

    using unix socket for connection to the database

--- System/DatabaseConnector.php
+++ System/DatabaseConnector.php
@@ -11,10 +11,12 @@ class DatabaseConnector {
         $db   = getenv('DB_DATABASE');
         $user = getenv('DB_USERNAME');
         $pass = getenv('DB_PASSWORD');
+        // $dns = "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$db";
+        $dns = 'mysql:dbname=' . $host . ';unix_socket=/var/run/mysqld/mysqld.sock';
 
         try {
             $this->dbConnection = new \PDO(
-                "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$db",
+                $dns,
                 $user,
                 $pass,
                 array (
